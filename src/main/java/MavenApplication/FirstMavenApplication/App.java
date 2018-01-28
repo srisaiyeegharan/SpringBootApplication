54a730fa8ce4c1275d71746f0d716bc971714fd0
Srisaiyeegharan Kidnapillai <100680347@student.swin.edu.au>
2018-01-29 09:37:56 +1100

    Changed App.class

--- src/main/java/MavenApplication/FirstMavenApplication/App.java
+++ src/main/java/MavenApplication/FirstMavenApplication/App.java
@@ -2,16 +2,27 @@ package MavenApplication.FirstMavenApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
+    }
+    
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 }
